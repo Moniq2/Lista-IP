cdf8d7556b59441ba8caaa4f9d704563d6147ce2
Moniq <138322961+Moniq2@users.noreply.github.com>
2024-12-09 14:35:14 -0300

    A

---
+++ Q1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>  
+
+/* 1. Escrever um algoritmo que lê 5 valores para a, um de cada vez, e conta quantos destes
+valores são negativos, escrevendo esta informação. */
+
+int main(){
+    int i, j;
+        for(i=0;i<=5;i++){
+            scanf("%d", j);
+        }
+    return 0;
+}
+ 
