e4749ee35e5918f94267ef7c3029d7822ee1bbc3
Moniq <138322961+Moniq2@users.noreply.github.com>
2024-12-09 14:42:55 -0300

    b

--- Q1.c
+++ Q1.c
@@ -4,10 +4,15 @@
 valores são negativos, escrevendo esta informação. */
 
 int main(){
-    int i, j;
+    int i, j, negativ;
         for(i=0;i<=5;i++){
-            scanf("%d", j);
+            scanf("%d", &j);
+            if (j < 0)
+            {
+                negativ++;
+            }
         }
+        printf("%d", negativ);
     return 0;
 }
  
